15d8be0dd6af8f88a7e139ef65dbdee4f47ceb71
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-08-01 19:31:26 +0530

    background-setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
-
+import background from './back.mp4';
 function App() {
   return (
+    <>
     <div className="App">
-      Hey its saksham
+       <div className='overlay'></div>
+       <div className='main'>
+       <video autoPlay loop muted className='video'>
+        <source
+          src={background}
+          type="video/mp4"
+        />
+      </video>
+      </div>
+      <div className='content'>
+        
+      </div>
     </div>
+    </>
   );
 }
 
