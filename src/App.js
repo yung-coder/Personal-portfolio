3ab1e39583155e92338266341c91780977241617
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-08-09 12:59:49 +0530

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import {  Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Layout from './components/Layout';
-import Sidebar from "./components/Sidebar/index";
 function App() {
   return (
     <>
--- src/components/Layout/Sidebar.jsx
+++ src/components/Layout/Sidebar.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import "./Sidebar.scss";
 import logoS from '../../assets/images/logo-s.png'
 import logoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 const Sidebar = () => {
   return (
-    <div>
        <div className="nav-bar">
         <Link className="logo" to="/">
           <img src={logoS} alt="logo" />
           <img src={logoSubtitle} alt="Slobodan" className="sub-logo" />
         </Link>
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink exact="true" activeclassname="active"  className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a target="blank_" rel='noreferrer' href='https://www.linkedin.com/in/saksham-chandel-yungcoder/'> 
+              <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a target="blank_" rel='noreferrer' href='https://www.linkedin.com/in/saksham-chandel-yungcoder/'> 
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+          </li>
+          <li>
+            <a target="blank_" rel='noreferrer' href='https://www.linkedin.com/in/saksham-chandel-yungcoder/'> 
+              <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+            </a>
+          </li>
+        </ul>
       </div>
-    </div>
   )
 }
 
