2623a26c28b35598b80a689d2627c6d7fdd98c21
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-08-09 14:42:13 +0530

    intro

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import {  Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Layout from './components/Layout';
 import Home from './components/Home';
+import Sidebar from "./components/Sidebar/Sidebar";
 function App() {
   return (
     <>
+      <div className="App">
+      <Sidebar />
       <Routes>
-        <Route path="/" element={<Layout/>}></Route>
-        {/* <Route index element={<Home />}></Route> */}
+        {/* <Route path="/" element={<Layout/>}></Route> */}
+        <Route index element={<Home />}></Route>
       </Routes>
+      </div>
     </>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,21 @@
-
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/logo-s.png'
+import './index.scss'
 const Home =()=>{
 
     return(
         <>
-        <div>
-            hey
+        <div className="contanier home-page">
+            <div className="text-zone">
+                <h1>Hi , <br /> I'm
+                <img src={LogoTitle} alt="developer" />
+                Saksham
+                <br />
+                web developer
+                </h1>
+                <h2>Frontend developer / Javascript </h2>
+                <Link to='/contact' className='flag-button'>CONTACT ME</Link>
+            </div>
         </div>
         </>
     )
