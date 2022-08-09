8df669b67cc94fb556611b0e896bfb58fd393350
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-08-09 15:28:29 +0530

    home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom'
 import LogoTitle from '../../assets/images/logo-s.png'
 import './index.scss'
+import Logo from './logo'
 const Home =()=>{
 
     return(
@@ -9,13 +10,14 @@ const Home =()=>{
             <div className="text-zone">
                 <h1>Hi , <br /> I'm
                 <img src={LogoTitle} alt="developer" />
-                Saksham
+                aksham
                 <br />
                 web developer
                 </h1>
                 <h2>Frontend developer / Javascript </h2>
                 <Link to='/contact' className='flag-button'>CONTACT ME</Link>
             </div>
+            <Logo />
         </div>
         </>
     )
