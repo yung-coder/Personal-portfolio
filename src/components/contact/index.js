81e042b1bdaa632c800d65472e1da906a93f20dd
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-08-11 19:40:55 +0530

    email sending

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,8 +1,33 @@
+import { EmailJSResponseStatus } from "@emailjs/browser";
 import { Input } from "postcss";
+import { useRef } from "react";
 import Loader from "react-loaders";
+import emailjs from '@emailjs/browser'
 import "./index.scss";
 
 const Contact = () => {
+  const refForm = useRef();
+
+  const sendEmail =(e) =>{
+     e.preventDefault()
+
+     emailjs
+       .sendForm(
+          'service_ed7a0i9',
+          'template_3krgw0t',
+          refForm.current,
+          'mW7tMDSy2F8rUY1Ig'
+       )
+       .then(
+         () => {
+           alert('Message send!');
+           window.location.reload(false)
+         },
+         ()=>{
+           alert('Failed to send');
+         }
+       )
+  }
   return (
     <>
       <div contanier contact-page>
@@ -14,7 +39,7 @@ const Contact = () => {
             Voluptatum incidunt harum maiores. Architecto, illo.
           </p>
         <div className="contact-form">
-          <form>
+          <form ref={refForm} onSubmit={sendEmail}>
             <ul>
               <li className="half">
                 <input type="text" name="name" placeholder="Name" required />
@@ -38,7 +63,7 @@ const Contact = () => {
                 ></textarea>
               </li>
               <li>
-                <input type="sumbit" className="flat-button" value="SEND" />
+                <input type="submit" className="flat-button" value="SEND" />
               </li>
             </ul>
           </form>
