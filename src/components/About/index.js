ffdf16a53164661ec4ea653a246f3b7a7c78bf3c
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-08-14 16:07:37 +0530

    about refactor

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,47 +1,179 @@
-import { faCss3, faFigma, faHtml5, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
-import { faGift } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Loader from 'react-loaders';
-import './index.scss';
+import {
+  faCss3,
+  faFigma,
+  faHtml5,
+  faJs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+import { faGift } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Loader from "react-loaders";
+import "./index.scss";
 const About = () => {
-    return (
-        <>
-        <div className='container about-page'>
-          <div className='text-zone'>
-            <h1>
-                About me
-            </h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, distinctio alias? Tenetur neque a vitae odio aspernatur in officiis excepturi ea ipsum ratione.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam debitis porro quod saepe iste. Quia, accusantium placeat. Exercitationem illo eveniet nam cum commodi!</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur maxime itaque, debitis tempora quia doloremque!</p>
-          </div>
-          <div className='stage-cube-cont'>
-            <div className='cubespinner'>
-                <div className='face1'>
-                    <FontAwesomeIcon icon={faReact} color="blue" />
-                </div>
-                <div className='face2'>
-                    <FontAwesomeIcon icon={faHtml5} color="orange" />
-                </div>
-                <div className='face3'>
-                    <FontAwesomeIcon icon={faCss3} color="blue" />
-                </div>
-                <div className='face4'>
-                    <FontAwesomeIcon icon={faJs} color="yellow" />
-                </div>
-                <div className='face5'>
-                    <FontAwesomeIcon icon={faFigma} color="pink" />
-                </div>
-                <div className='face6'>
-                    <FontAwesomeIcon icon={faGift} color="black" />
-                </div>
+  return (
+    <>
+      <div class="content">
+        <div class="card">
+          <div class="firstinfo">
+            <img src="https://randomuser.me/api/portraits/lego/6.jpg" />
+            <div class="profileinfo">
+              <h1>Francesco Moustache</h1>
+              <h3>Python Ninja</h3>
+              <p class="bio">
+                Lived all my life on the top of mount Fuji, learning the way to
+                be a Ninja Dev.
+              </p>
             </div>
           </div>
         </div>
-        <Loader type='pacman'/>
-        </>
-    )
-}
-
+        <div class="badgescard">
+          {" "}
+          <span class="devicons devicons-django"></span>
+          <span class="devicons devicons-python"> </span>
+          <span class="devicons devicons-codepen"></span>
+          <span class="devicons devicons-javascript_badge"></span>
+          <span class="devicons devicons-gulp"></span>
+          <span class="devicons devicons-angular"></span>
+          <span class="devicons devicons-sass"></span>
+        </div>
+      </div>
+      <div className="aboutme">
+        <h1>About me</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis
+          expedita a quibusdam nulla veritatis similique enim accusantium autem
+          quae. Aspernatur illum voluptatem, non quisquam iste magnam illo
+          reprehenderit, adipisci in mollitia alias temporibus nisi!
+        </p>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
+          harum eveniet, doloremque placeat est odit quas voluptatum quis vero
+          officia ex quos natus corrupti, facere repellendus in, praesentium
+          officiis et quo sed.
+        </p>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus,
+          voluptatem voluptatibus at vero labore minus excepturi cum repudiandae
+          eum tempora sit aperiam explicabo maiores eos doloribus aliquid modi
+          doloremque architecto.
+        </p>
+        <hr />
+      </div>
+      <div className="skilldection">
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+        <div class="circle-wrap">
+          <div class="circle">
+            <div class="mask half">
+              <div class="fill"></div>
+            </div>
+            <div class="mask full">
+              <div class="fill"></div>
+            </div>
+            <div class="inside-circle"> 75% </div>
+          </div>
+        </div>
+     </div>
+      <Loader type="pacman" />
+    </>
+  );
+};
 
 export default About;
