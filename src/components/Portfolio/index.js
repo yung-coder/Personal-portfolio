5d9f1ef6fb822b02a2ce78b7ea243925ea1f2373
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-08-17 14:54:37 +0530

    modify

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -7,14 +7,17 @@ const index = () => {
         <div class="card">
           <h3 class="title">Card 1</h3>
           <div class="bar">
-            <div class="emptybar"></div>
-            <div class="filledbar"></div>
+          <div className="content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum libero, unde commodi reprehenderit labore impedit nulla dolore iusto odit est nihil nobis hic asperiores sapiente deleniti accusantium corrupti at quos omnis, tempora mollitia? Debitis itaque facere dolore accusamus maxime nam placeat quae dignissimos ut sequi.
           </div>
-          <div class="circle">
+            {/* <div class="emptybar"></div>
+            <div class="filledbar"></div> */}
+          </div>
+          {/* <div class="circle">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
               <circle class="stroke" cx="60" cy="60" r="50" />
             </svg>
-          </div>
+          </div> */}
         </div>
         <div class="card">
           <h3 class="title">Card 2</h3>
