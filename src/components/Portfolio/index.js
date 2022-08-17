8d064c4d5a3a3bd2733d0fdb6ec9202a13d0a1ad
Saksham chandel <94097778+yung-coder@users.noreply.github.com>
2022-08-17 13:37:57 +0530

    portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./index.css";
+import "./index.scss";
 const index = () => {
   return (
     <div className="holder">
@@ -76,8 +76,44 @@ const index = () => {
             </svg>
           </div>
         </div>
+        <div class="card">
+          <h3 class="title">Card 6</h3>
+          <div class="bar">
+            <div class="emptybar"></div>
+            <div class="filledbar"></div>
+          </div>
+          <div class="circle">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <circle class="stroke" cx="60" cy="60" r="50" />
+            </svg>
+          </div>
+        </div>
+        <div class="card">
+          <h3 class="title">Card 6</h3>
+          <div class="bar">
+            <div class="emptybar"></div>
+            <div class="filledbar"></div>
+          </div>
+          <div class="circle">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <circle class="stroke" cx="60" cy="60" r="50" />
+            </svg>
+          </div>
+        </div>
+        <div class="card">
+          <h3 class="title">Card 6</h3>
+          <div class="bar">
+            <div class="emptybar"></div>
+            <div class="filledbar"></div>
+          </div>
+          <div class="circle">
+            <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <circle class="stroke" cx="60" cy="60" r="50" />
+            </svg>
+          </div>
+        </div>
       </div>
-    </div>
+      </div>
   );
 };
 
