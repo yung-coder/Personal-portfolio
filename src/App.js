8fa4f9cce295fb2cb961d297482bee08b17a9246
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-13 22:47:37 +0530

    development start

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Home from './components/Home';
+import Home from "./components/Home";
 import Sidebar from "./components/Sidebar/Sidebar";
 import About from "./components/About";
 import Contact from "./components/contact";
-import Portfolio from './components/Portfolio';
+import Portfolio from "./components/Portfolio";
 function App() {
   return (
     <>
-      <div className="App">
-      <Sidebar />
-      <Routes>
-        <Route index element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-        <Route path="/portfolio" element={<Portfolio />}></Route>
-      </Routes>
-      </div>
+      <h1>UNDER DEVELOPMENT</h1>
+      <div className="App"></div>
     </>
   );
 }
