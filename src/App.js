e65a59f7c1b429f4564b44256dfebe0cffb53d83
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-13 22:52:10 +0530

    repair

--- src/App.js
+++ src/App.js
@@ -1,10 +1,4 @@
-import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Home from "./components/Home";
-import Sidebar from "./components/Sidebar/Sidebar";
-import About from "./components/About";
-import Contact from "./components/contact";
-import Portfolio from "./components/Portfolio";
 function App() {
   return (
     <>
