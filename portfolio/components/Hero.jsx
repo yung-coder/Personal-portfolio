307e1af07388342ff810180fb482082c6344a583
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-14 23:48:51 +0530

    styles

---
+++ portfolio/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { styles } from "../src/app/style";
+
+const Hero = () => {
+  return (
+    <section className={`relative w-full h-screen mx-auto border`}> 
+      <div
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915EFF]" />
+          <div className="w-1 sm:h-80 h-40 bg-[#915EFF]" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915EFF]">Adrian</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop 3D visuals, user <br className="sm:block hidden" />
+            interfaces and web applications
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -3,14 +3,16 @@ import { Inter } from "next/font/google";
 import styles from "./page.module.css";
 import Navbar from "../../components/Navbar";
 import Pic from "../../public/assests/two.jpg";
+import Hero from "../../components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <div className="w-screen h-screen bg-[#000]">
+      <div className="w-screen h-screen bg-[#000] border border-red-700">
         <Navbar />
+        <Hero />
       </div>
     </main>
   );
