05f34626aecf3cd0755f514819b3737d81006e95
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-18 19:00:45 +0530

    contact raw

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "../../components/Navbar";
 import Hero from "../../components/Hero";
 import Skills from "../../components/Skills";
+import Contact from "../../components/Contact";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +16,7 @@ export default function Home() {
         <Hero />
       </div>
       <Skills />
+      <Contact />
     </main>
   );
 }
