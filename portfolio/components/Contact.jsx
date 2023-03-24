329f389a97281a6c6607cc7f7fe9f9e37dad3e34
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-24 22:31:19 +0530

    ui improve

--- portfolio/components/Contact.jsx
+++ portfolio/components/Contact.jsx
@@ -222,6 +222,27 @@ const Contact = () => {
         pauseOnHover
         theme="dark"
       />
+      <motion.div
+        className="w-full h-16 flex justify-center items-center"
+        variants={fadeIn("up", "tween", 0.2, 1)}
+      >
+        <a href="#home">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-8 h-8 text-white bg-[#151030] rounded-lg p-2 animate-bounce"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.25 6.75L12 3m0 0l3.75 3.75M12 3v18"
+            />
+          </svg>
+        </a>
+      </motion.div>
     </section>
   );
 };
