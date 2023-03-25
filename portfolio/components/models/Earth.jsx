b4a142a7ae2d1127efb6291129510650f470515e
Saksham Chandel <chandel.sanjeev5@gmail.com>
2023-03-25 14:55:15 +0530

    cleanup

--- portfolio/components/models/Earth.jsx
+++ portfolio/components/models/Earth.jsx
@@ -8,14 +8,14 @@ const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive  object={earth.scene} scale={1.5} position-y={0} rotation-y={0} />
   );
 };
 
 const EarthCanvas = () => {
   return (
     <Canvas
-      shadows
+      shadows={true}
       frameloop='demand'
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
