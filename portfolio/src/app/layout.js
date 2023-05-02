d4d295145f79cfe0dd0b5c8b20aec240505e1351
yung-coder <chandel.sanjeev5@gmail.com>
2023-05-03 00:35:55 +0530

    og-image

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -9,18 +9,16 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta property="og:title" content="Example Page"></meta>
+      <meta property="og:title" content="Portfolio Saksham Chandel"></meta>
       <meta
         name="image"
         property="og:image"
-        content="https://learn.g2.com/hubfs/IT%20Portfolio%20Management@2x.png"
+        content="https://assets.justinmind.com/wp-content/uploads/2021/11/web-design-portfolio-768x492.png"
       ></meta>
       <meta
         property="og:description"
-        content="This is just an example page."
+        content="Hey, its my portfolio page"
       ></meta>
-      <meta property="og:url" content="https://example.com/page.html"></meta>
-      <meta name="twitter:card" content="summary_large_image"></meta>
       <body>{children}</body>
     </html>
   );
