e2c110783c76d22e0593ec9f5d8562189098776c
yung-coder <chandel.sanjeev5@gmail.com>
2023-05-03 00:14:36 +0530

    og-image

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,17 +1,24 @@
 import "./globals.css";
 
-export const metadata = {
-  title: "Portfolio",
-  description: "Made by Saksham Chandel",
-};
+// export const metadata = {
+//   title: "Portfolio",
+//   description: "Made by Saksham Chandel",
+// };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta property="og:title" content="Example Page"></meta>
       <meta
         property="og:image"
         content="https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2022/04/web-developer-portfolio.webp"
       ></meta>
+      <meta
+        property="og:description"
+        content="This is just an example page."
+      ></meta>
+      <meta property="og:url" content="https://example.com/page.html"></meta>
+      <meta name="twitter:card" content="summary_large_image"></meta>
       <body>{children}</body>
     </html>
   );
