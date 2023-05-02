afa68753c8323acaafa0c871d7839377b47b6406
yung-coder <chandel.sanjeev5@gmail.com>
2023-05-03 00:05:52 +0530

    og-image

--- portfolio/src/app/layout.js
+++ portfolio/src/app/layout.js
@@ -1,14 +1,18 @@
-import './globals.css'
+import "./globals.css";
 
 export const metadata = {
-  title: 'Portfolio',
-  description: 'Made by Saksham Chandel',
-}
+  title: "Portfolio",
+  description: "Made by Saksham Chandel",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta
+        property="og:image"
+        content="https://www.hostinger.com/tutorials/wp-content/uploads/sites/2/2022/04/web-developer-portfolio.webp"
+      ></meta>
       <body>{children}</body>
     </html>
-  )
+  );
 }
